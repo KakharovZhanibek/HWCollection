c9801f259d560652d5c04e438536a51c8dd77437
КахаровЖ <@205_10>
2019-03-06 14:45:53 +0600

    ok

--- HWCollection/Program.cs
+++ HWCollection/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,7 +60,7 @@ namespace HWCollection
                 numbers.Add(rnd.Next(0, 15) + d);
                 Console.WriteLine(" " + numbers[i]);
             }
-            Console.WriteLine("__________________\nСреднее арифметическое :"+numbers.Sum()/numbers.Count+"\n__________________\nБольше среднего арифметического");
+            Console.WriteLine("__________________\nСреднее арифметическое :" + numbers.Sum() / numbers.Count + "\n__________________\nБольше среднего арифметического");
             for (int i = 0; i < numbers.Count; i++)
             {
                 if (numbers[i] > numbers.Sum() / numbers.Count)
@@ -68,6 +69,83 @@ namespace HWCollection
                 }
             }
         }
+        public static void ReverseLetters()
+        {
+            string path;
+            char[] res; char[] temp;
+            Console.WriteLine("Введите путь к файлу");
+            path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    temp = sr.ReadToEnd().ToCharArray();
+                    res = new char[temp.Length];
+                }
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    Console.Write("{0} ", temp[i]);
+                }
+                Console.WriteLine("\n___________________");
+
+                int x = temp.Length - 1;
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    res[i] = temp[x];
+                    x--;
+                }
+
+                for (int i = 0; i < res.Length; i++)
+                {
+                    Console.Write("{0} ", res[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+       /* public static void VowelsFirst()
+        {
+            string path;
+            string temp;
+
+            Queue<string> Vowels = new Queue<string>();
+            Queue<string> Consonants = new Queue<string>();
+
+            Console.WriteLine("Введите путь к файлу");
+            path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    temp = sr.ReadToEnd();
+                    temp.ToLower();
+                }
+                Vowels.SelectMany(n => n
+                .Split()
+                .Where(x => x.StartsWith("a") || x.StartsWith("e") || x.StartsWith("i") || x.StartsWith("o") || x.StartsWith("u")));
+
+                Consonants.SelectMany(n => n
+                .Split()
+                .Where(x => x.StartsWith("b") || x.StartsWith("c") || x.StartsWith("d") || x.StartsWith("f") || x.StartsWith("g")
+                || x.StartsWith("h") || x.StartsWith("j") || x.StartsWith("k") || x.StartsWith("l") || x.StartsWith("m")
+                || x.StartsWith("n") || x.StartsWith("p") || x.StartsWith("q") || x.StartsWith("r") || x.StartsWith("s")
+                || x.StartsWith("t") || x.StartsWith("v") || x.StartsWith("w") || x.StartsWith("x") || x.StartsWith("y")
+                || x.StartsWith("z")));
+
+                Console.WriteLine("\n\n");
+                Console.WriteLine(string.Join(" ", Vowels));
+                Console.WriteLine(string.Join(" ", Consonants));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }*/
+        
 
         static void Main(string[] args)
         {
@@ -80,6 +158,8 @@ namespace HWCollection
             Console.WriteLine("№3------------------\n\n");
             SredAr();
             Console.WriteLine("\n\n-------------------\n\n");
+            Console.WriteLine("№4------------------\n\n");
+            ReverseLetters();
         }
     }
 }
